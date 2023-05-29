0df4afbebbc538644f8f7c7942138d057aadbfa1
Isaac Boothe <iboothe@uncc.edu>
2023-05-29 18:09:45 -0400

    Initial Commit

---
+++ UserSampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserSampleApp
+//
+//  Created by Isaac Boothe on 5/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserSampleApp/UserSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserSampleAppApp.swift
+//  UserSampleApp
+//
+//  Created by Isaac Boothe on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct UserSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
